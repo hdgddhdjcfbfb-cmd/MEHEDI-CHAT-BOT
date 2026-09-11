85c07e6f6a8448511180757cf242ea0bef2526eb
hdgddhdjcfbfb-cmd <hdgddhdjcfbfb@gmail.com>
2026-09-11 21:09:15 +0600

    Update Admin.js

--- Script/commands/Admin.js
+++ Script/commands/Admin.js
@@ -35,7 +35,7 @@ module.exports.run = async function({ api, event }) {
 │ 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
 ├───────────────
 │📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸:
-│https://fb.com/100009335118902
+│https://fb.com/61594273261792
 │💬 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽:
 │https://wa.me/01602166603
 └───────────────⭓
